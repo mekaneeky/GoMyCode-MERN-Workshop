f8a6e8280c25631fed7fe18d576da530515c3454
mekaneeky <alizawahry1@gmail.com>
2021-06-14 12:53:33 +0200

    Look at how nice my Signup page is...Amar msa !

--- contact-mern/src/components/SignUp.js
+++ contact-mern/src/components/SignUp.js
@@ -4,11 +4,20 @@ function SignUpForm() {
   return (
     <Container>
       <Form>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Username"
+            name="username"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
             name="username"
           />
         </Form.Group>
@@ -21,6 +30,16 @@ function SignUpForm() {
             placeholder="Password"
           />
         </Form.Group>
+
+        <Form.Group controlId="formBasicPasswordConfirm">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            name="password_confirm"
+            type="password"
+            placeholder="Re-enter Password"
+          />
+        </Form.Group>
+
         <Button variant="primary" type="submit">
           SignUp
         </Button>
