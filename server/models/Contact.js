d17d688e015063b271932f60bbe9fc3ba10b2a06
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-06-15 02:47:02 +0200

    connect with atlas db

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,23 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-require('dotenv').config();
+require("dotenv").config();
 
-mongoose.connect("mongodb+srv://mern:mern1234@cluster0.ikppq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true},  function(error){
-    console.log("open done"+mongoose.connection.host+"\t"+ mongoose.connection.port)
-      if(error){
-        console.log("error"+error);
-      }else{
-      }
-    });
+mongoose.connect(
+  "mongodb+srv://mern-app:mern1234@cluster0.ikppq.mongodb.net/mern-contact?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (error) {
+    console.log(
+      "open done" + mongoose.connection.host + "\t" + mongoose.connection.port
+    );
+    if (error) {
+      console.log("error" + error);
+    } else {
+    }
+  }
+);
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 
 let contactSchema = new Schema({
-    name:  String, // String is shorthand for {type: String}
-    number: String,
-    email: String
+  name: String, // String is shorthand for {type: String}
+  number: String,
+  email: String,
 });
 
-let Contact = mongoose.model('Contact', contactSchema);
+let Contact = mongoose.model("Contact", contactSchema);
 
-exports.Contact = Contact
+exports.Contact = Contact;
